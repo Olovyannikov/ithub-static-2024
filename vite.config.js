80b62b3de654649b287905dbf429a25a5b47725f
Ilya Olovyannikov <excaliburgyro@gmail.com>
2024-03-25 00:36:27 +0300

    feat(hero): add hero block

--- vite.config.js
+++ vite.config.js
@@ -2,23 +2,35 @@ import { defineConfig } from 'vite';
 import vituum from 'vituum';
 import pug from '@vituum/vite-plugin-pug';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
-
-import path from 'path';
-
-const { resolve } = path;
+import createSvgSpritePlugin from 'vite-plugin-svg-spriter'
 
 export default defineConfig(({ mode }) => {
     console.log({ mode });
 
     return {
         base: mode === 'development' ? './' : '/ithub-static-2024',
+        css: {
+            devSourcemap: mode === 'development',
+            preprocessorOptions: {
+                scss: {
+                    additionalData: [
+                        '@import "./src/scss/general/_grid.scss";',
+                        ''
+                    ].join('\n')
+                }
+            }
+        },
         plugins: [
             vituum({
                 imports: {
                     filenamePattern: {
+                        '+.css': [],
                         '+.scss': 'src/scss',
                     },
                 },
+            }),
+            createSvgSpritePlugin({
+                svgFolder: './public/icons'
             }),
             pug({
                 root: './src',
